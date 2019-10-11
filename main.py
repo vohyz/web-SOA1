eb52175d746ae7459017b2e430a69e7dfd2793e4
vohyz <531322336@qq.com>
2019-10-11 09:58:51 +0800

    feature - 初始化

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, session
+import requests
+import json
+
+app = Flask(__name__)
+'''
+url1 = 'http://www.baidu.com/s'
+r = requests.post(url1)
+print(r.status_code)
+response = r.json()
+
+print(response.keys())
+'''
+@app.route('/', methods=["GET","POST"])
+def hello_world():
+    if request.method == "POST":
+        form_data = request.form
+        city = form_data.get("search")
+        Data = {"city" : city}
+    return render_template('index.html', Data = Data)
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1",port=5000,debug = True)
